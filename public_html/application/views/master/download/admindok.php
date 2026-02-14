fc8f9595b6dbf73372e0c6c2c8f7136faff4f875
pusi <pusirdp@gmail.com>
2026-02-14 11:22:27 +0700

    tes hapus

--- public_html/application/views/master/download/admindok.php
+++ public_html/application/views/master/download/admindok.php
@@ -529,6 +529,11 @@
 			"ordering": false // false to disable sorting (or any other option)
 		});
 		$('.dataTables_length').addClass('bs-select');
+
+
+		$(document).on('click','.hapus',function(){
+			alert('tes hapus');
+		});
 	});
 
 	$(document).ready(function(){
